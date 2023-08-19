03ed7e7d95828ac29ce3ff7f1768f3b8a29f684c
Jhonatan <jhonatanruivo@outlook.com>
2023-08-19 16:42:58 -0300

    feat: upload image to tmp folder

--- src/routes/dishes-routes.js
+++ src/routes/dishes-routes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express')
-
-const dishesRouter = Router()
+const multer = require('multer')
+const uploadConfig = require('../configs/upload')
 
 const DishesController = require('../controllers/DishesController')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+
+const dishesRouter = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const dishesController = new DishesController()
 
@@ -14,5 +17,9 @@ dishesRouter.get('/:id', dishesController.show)
 dishesRouter.delete('/:id', dishesController.delete)
 dishesRouter.get('/', dishesController.index)
 dishesRouter.put('/:id', dishesController.update)
+dishesRouter.patch('/imagedish', upload.single('image'), (request, response) => {
+  console.log(request.file.filename)
+  response.json()
+})
 
 module.exports = dishesRouter
